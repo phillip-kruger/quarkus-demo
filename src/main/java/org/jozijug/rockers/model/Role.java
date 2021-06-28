118792c92eafd31cf288b4c8eab28f2ce6cd40eb
Phillip Kruger <phillip.kruger@gmail.com>
2021-06-28 10:05:42 +0200

    init Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/org/jozijug/rockers/model/Role.java
+++ src/main/java/org/jozijug/rockers/model/Role.java
@@ -1,11 +1,11 @@
 package org.jozijug.rockers.model;
 
 public enum Role {
-    Lead_Guitar, // 0
-    Bass_Guitar, // 1
-    Rhythm_Guitar, // 2
-    Drums, // 3
-    Keyboards, // 4
-    Lead_Vocals, // 5
-    Backing_Vocals // 6
+    Lead_Guitar,
+    Bass_Guitar,
+    Rhythm_Guitar,
+    Drums,
+    Keyboards,
+    Lead_Vocals,
+    Backing_Vocals
 }
